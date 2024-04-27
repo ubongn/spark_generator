7de2ad815abc91a406dcdc3375050332d3d9d031
ubong <ubongnt@gmail.com>
2024-04-27 07:40:43 +0100

    dfgenerator

--- test/test_csvgen.py
+++ test/test_csvgen.py
@@ -13,18 +13,15 @@ class TestCsvgen(unittest.TestCase):
         self.csvgen = CsvGenerator(self.column_names_types)
 
     def test_generate_data(self):
-        # Generate sample data
+        
         rows_num = 2
         sample_data = self.csvgen.generate_data(rows_num)
         
-        # Check if data is generated
         self.assertIsNotNone(sample_data)
         
-        # Check if data contains the expected columns
         expected_columns = self.column_names_types.keys()
         self.assertCountEqual(sample_data.keys(), expected_columns)
         
-        # Check data types of each column
         for col, dtype in self.column_names_types.items():
             for value in sample_data[col]:
                 if dtype == "str":
@@ -34,12 +31,10 @@ class TestCsvgen(unittest.TestCase):
                 elif dtype == "float":
                     self.assertIsInstance(value, float)
                 elif dtype == "date":
-                    self.assertIsInstance(value, str)  # Date is represented as string
-                    # Add additional date format validation if required
+                    self.assertIsInstance(value, str)  
                 elif dtype == "boolean":
                     self.assertIsInstance(value, bool)
         
-        # Check if the number of rows generated matches the expected number
         for col, values in sample_data.items():
             self.assertEqual(len(values), rows_num)
 
